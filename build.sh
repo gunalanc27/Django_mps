1d218f5074ddd2780364b41cb0c6d5ba1e0eada1
TNlucfer01 <ihdaa2006@gmail.com>
2026-04-13 19:24:24 +0530

    update build commnd

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ echo "Building Vercel project..."
 # Python Version
 python3 -m pip install -r requirements.txt
 
+echo "Building Tailwind CSS..."
+# Navigate to the theme app and build CSS
+cd theme/static_src
+npm install
+npm run build
+cd ../..
+
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
